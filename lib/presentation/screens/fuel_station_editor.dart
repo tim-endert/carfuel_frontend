365b51df732f220fda374115a9bb8ff459b43404
Tim Endert <enderttim@gmail.com>
2024-07-26 14:24:22 +0200

    bloclistener for editor

--- lib/presentation/screens/fuel_station_editor.dart
+++ lib/presentation/screens/fuel_station_editor.dart
@@ -1,4 +1,5 @@
 import 'package:carfuel_frontend/bloc/fuel_station_detail/fuel_station_detail_bloc.dart';
+import 'package:carfuel_frontend/bloc/fuel_station_overview_bloc/fuel_station_overview_bloc.dart';
 import 'package:carfuel_frontend/data/models/fuel_station.dart';
 import 'package:carfuel_frontend/presentation/widgets/pump_field_set.dart';
 import 'package:flutter/material.dart';
@@ -38,114 +39,148 @@ class _FuelStationEditorState extends State<FuelStationEditor> {
 
   @override
   Widget build(BuildContext context) {
-    return FormBuilder(
-      clearValueOnUnregister: true,
-      key: _formKey,
-      child: Scaffold(
-        appBar: AppBar(
-          title:
-              Text(widget.station != null ? "Edit station" : "Create station"),
-        ),
-        body: CustomScrollView(slivers: [
-          SliverToBoxAdapter(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(
-                vertical: 18,
-                horizontal: 21,
+    return BlocListener<FuelStationDetailBloc, FuelStationDetailState>(
+      listener: (context, state) {
+        if (state is FuelStationDetailLoading) {
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => const Center(
+              child: SizedBox(
+                height: 24,
+                width: 24,
+                child: CircularProgressIndicator(),
               ),
-              child: Column(
-                children: [
-                  FormBuilderTextField(
-                    name: 'name',
-                    initialValue: widget.station?.name,
-                    decoration:
-                        const InputDecoration(labelText: 'Station Name'),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(),
-                    ]),
-                  ),
-                  FormBuilderTextField(
-                    name: 'address',
-                    initialValue: widget.station?.address,
-                    decoration: const InputDecoration(labelText: 'Address'),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(),
-                    ]),
-                  ),
-                  FormBuilderTextField(
-                    name: 'city',
-                    initialValue: widget.station?.city,
-                    decoration: const InputDecoration(labelText: 'City'),
-                    validator: FormBuilderValidators.compose([
-                      FormBuilderValidators.required(),
-                    ]),
-                  ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Flexible(
-                        child: FormBuilderTextField(
-                          name: 'latitude',
-                          initialValue: widget.station?.latitude.toString(),
-                          decoration: const InputDecoration(labelText: 'Lat'),
-                          validator: FormBuilderValidators.compose([
-                            FormBuilderValidators.required(),
-                            FormBuilderValidators.numeric()
-                          ]),
-                        ),
-                      ),
-                      Flexible(
-                        child: FormBuilderTextField(
-                          name: 'longitude',
-                          initialValue: widget.station?.longitude.toString(),
-                          decoration: const InputDecoration(labelText: 'Lng'),
-                          validator: FormBuilderValidators.compose([
-                            FormBuilderValidators.required(),
-                            FormBuilderValidators.numeric()
-                          ]),
-                        ),
-                      ),
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            ),
+          );
+        }
+
+        if (state is FuelStationDetailFailure) {
+          Navigator.pop(context);
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Operation Failed: ${state.errorMessage}'),
+          ));
+        }
+
+        if (state is FuelStationDetailSuccess) {
+          context.read<FuelStationOverviewBloc>().add(FetchFuelStations());
+          Navigator.popUntil(context, ModalRoute.withName("/"));
+
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+              content: Text('Success'),
+            ),
+          );
+        }
+      },
+      child: FormBuilder(
+        clearValueOnUnregister: true,
+        key: _formKey,
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+                widget.station != null ? "Edit station" : "Create station"),
+          ),
+          body: CustomScrollView(slivers: [
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 18,
+                  horizontal: 21,
+                ),
+                child: Column(
+                  children: [
+                    FormBuilderTextField(
+                      name: 'name',
+                      initialValue: widget.station?.name,
+                      decoration:
+                          const InputDecoration(labelText: 'Station Name'),
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(),
+                      ]),
+                    ),
+                    FormBuilderTextField(
+                      name: 'address',
+                      initialValue: widget.station?.address,
+                      decoration: const InputDecoration(labelText: 'Address'),
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(),
+                      ]),
+                    ),
+                    FormBuilderTextField(
+                      name: 'city',
+                      initialValue: widget.station?.city,
+                      decoration: const InputDecoration(labelText: 'City'),
+                      validator: FormBuilderValidators.compose([
+                        FormBuilderValidators.required(),
+                      ]),
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
                       children: [
-                        const Text("Pumps"),
-                        IconButton(
-                          onPressed: () => setState(
-                            () => pumpFields
-                                .add(PumpFieldSet(fieldId: newTextFieldId++)),
+                        Flexible(
+                          child: FormBuilderTextField(
+                            name: 'latitude',
+                            initialValue: widget.station?.latitude.toString(),
+                            decoration: const InputDecoration(labelText: 'Lat'),
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(),
+                              FormBuilderValidators.numeric()
+                            ]),
                           ),
-                          icon: const Icon(Icons.add),
+                        ),
+                        Flexible(
+                          child: FormBuilderTextField(
+                            name: 'longitude',
+                            initialValue: widget.station?.longitude.toString(),
+                            decoration: const InputDecoration(labelText: 'Lng'),
+                            validator: FormBuilderValidators.compose([
+                              FormBuilderValidators.required(),
+                              FormBuilderValidators.numeric()
+                            ]),
+                          ),
                         ),
                       ],
                     ),
-                  ),
-                  ...pumpFields
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(top: 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          const Text("Pumps"),
+                          IconButton(
+                            onPressed: () => setState(
+                              () => pumpFields
+                                  .add(PumpFieldSet(fieldId: newTextFieldId++)),
+                            ),
+                            icon: const Icon(Icons.add),
+                          ),
+                        ],
+                      ),
+                    ),
+                    ...pumpFields
+                  ],
+                ),
               ),
             ),
-          ),
-        ]),
-        bottomNavigationBar: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 36),
-            child: ElevatedButton(
-              onPressed: () {
-                if (_formKey.currentState!.validate()) {
-                  _formKey.currentState!.save();
-                  final formData = _formKey.currentState!.value;
+          ]),
+          bottomNavigationBar: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 36),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    _formKey.currentState!.save();
+                    final formData = _formKey.currentState!.value;
 
-                  context.read<FuelStationDetailBloc>().add(
-                      widget.station == null
-                          ? FuelStationCreateRequested(formData)
-                          : FuelStationUpdateRequested(
-                              formData, widget.station!.id));
-                }
-              },
-              child: Text(widget.station != null ? "Edit" : "Save"),
+                    context.read<FuelStationDetailBloc>().add(
+                        widget.station == null
+                            ? FuelStationCreateRequested(formData)
+                            : FuelStationUpdateRequested(
+                                formData, widget.station!.id));
+                  }
+                },
+                child: Text(widget.station != null ? "Edit" : "Save"),
+              ),
             ),
           ),
         ),
